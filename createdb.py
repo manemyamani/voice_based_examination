f7558621954441eb487794542d6c78ff25931e2f
yamini <manemyamani003@gmail.com>
2025-03-04 12:56:05 +0530

    added table

--- createdb.py
+++ createdb.py
@@ -13,6 +13,17 @@ def create_database():
             instructions_pdf TEXT NOT NULL
         )
     """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS questions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            serial_no INTEGER NOT NULL,
+            section TEXT NOT NULL,
+            question TEXT NOT NULL,
+            options TEXT NOT NULL, -- Store options as a JSON string or CSV format
+            marks INTEGER NOT NULL,
+            correct_answer TEXT NOT NULL
+        )
+    """)
 
     conn.commit()
     conn.close()
